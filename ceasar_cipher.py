22efeea81fcaff997f53dc69f41dbbdb4e1a0aca
Karol Kruszyński <90861840+karolkruszynski@users.noreply.github.com>
2023-03-11 17:52:12 +0100

    Adding user more user inteface option
    Added: 
    - Art Logo
    - Restart program option
    Debuging: 
    - Shifts greater than number of letters in alphabet

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,22 +1,33 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def ceasar(text,shift,direction):
     word = ""
     if direction == "decode":
         shift *= -1
     for i in text:
-        letter_nr = alphabet.index(i)
-        shift_letter_number = letter_nr + shift
-        if shift_letter_number > 25:
-            shift_letter_number -= 26
-            shifted_letter = alphabet[shift_letter_number]
-            word += shifted_letter
+        if i in alphabet:
+            letter_nr = alphabet.index(i)
+            shift_letter_number = letter_nr + shift
+            if shift_letter_number > 25:
+                shift_letter_number -= 26
+                shifted_letter = alphabet[shift_letter_number]
+                word += shifted_letter
+            else:
+                shifted_letter = alphabet[shift_letter_number]
+                word += shifted_letter
         else:
-            shifted_letter = alphabet[shift_letter_number]
-            word += shifted_letter
+            word += i
     print(f"The {direction}d text is {word}")
-ceasar(text,shift,direction)
+from art import logo
+print(logo)
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    ceasar(text,shift,direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
