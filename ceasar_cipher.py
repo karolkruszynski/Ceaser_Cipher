3318b2712e92c443dddb4c919d2afd0230617551
Karol Kruszyński <90861840+karolkruszynski@users.noreply.github.com>
2023-03-11 17:36:47 +0100

    Reorganising Code with one funct ceasar()

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -4,29 +4,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text,shift):
+def ceasar(text,shift,direction):
     word = ""
+    if direction == "decode":
+        shift *= -1
     for i in text:
-            letter_nr = alphabet.index(i)
-            shift_letter_number = letter_nr+shift
-            if shift_letter_number > 25:
-                shift_letter_number -= 26
-                shifted_letter = alphabet[shift_letter_number]
-                word += shifted_letter
-            else:
-                shifted_letter = alphabet[shift_letter_number]
-                word += shifted_letter
-    print(word)
-def decrypt(text,shift):
-    word = ""
-    for i in text:
-            letter_nr = alphabet.index(i)
-            shift_letter_number = letter_nr-shift
+        letter_nr = alphabet.index(i)
+        shift_letter_number = letter_nr + shift
+        if shift_letter_number > 25:
+            shift_letter_number -= 26
             shifted_letter = alphabet[shift_letter_number]
             word += shifted_letter
-    print(word)
-if direction == "encode":
-    encrypt(text,shift)
-elif direction == "decode":
-    decrypt(text,shift)
+        else:
+            shifted_letter = alphabet[shift_letter_number]
+            word += shifted_letter
+    print(f"The {direction}d text is {word}")
+ceasar(text,shift,direction)
